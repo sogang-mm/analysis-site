5cb413ef8e67cfee380ad23afe5408f49509b0ae
DESKTOP-TDE98GM\Shin <kepler92kr@gmail.com>
2018-03-26 14:32:02 +0900

    Local Image files will remove

--- ModuleCommunicator/beats.py
+++ ModuleCommunicator/beats.py
@@ -11,7 +11,7 @@ def delete_old_database(days=0):
     date_delta = datetime.timedelta(days)
     date_point = date_now - date_delta
 
-    old_database = models.ImageModel.objects.filter()#uploaded_date__lte=date_point)
+    old_database = models.ImageModel.objects.filter(uploaded_date__lte=date_point)
     old_database_count = old_database.count()
     for each_database in old_database:  # Delete local image files
         each_database.image.delete()
