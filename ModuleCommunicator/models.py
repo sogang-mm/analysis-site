c67cb0e32441abb03fd84bc8dca05110543896af
DESKTOP-TDE98GM\Shin <kepler92kr@gmail.com>
2018-05-05 14:28:00 +0900

    Dockerfile Edit -> Install wget, Expose 8000 port Module Communicator Model Structure Edit -> Increased flexibility

--- ModuleCommunicator/models.py
+++ ModuleCommunicator/models.py
@@ -4,9 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from rest_framework import exceptions
 from ModuleCommunicator import tasks
 from ModuleManager.models import *
-import json
+import json, ast
 
 
 class ImageModel(models.Model):
@@ -14,53 +15,63 @@ class ImageModel(models.Model):
     token = models.AutoField(primary_key=True)
     uploaded_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-    modules = models.TextField()
-    result = models.TextField(null=True)
+    modules = models.TextField(blank=True)
 
     def save(self, *args, **kwargs):
         super(ImageModel, self).save(*args, **kwargs)
+
         module_group_list = self.modules.split(',')
+        module_set = ModuleModel.objects.get_queryset()
 
-        result_model_list = []
-        for module_group_name in module_group_list:
+        for module_group in module_group_list:
             try:
-                module_group = ModuleGroupModel.objects.get(name=module_group_name.strip())
+                modules_in_group = ModuleGroupModel.objects.get(name=module_group.strip())
+                module_set.union(modules_in_group.modules.all())
             except:
-                self.result = u"Module not found. Please check and send again."
-                super(ImageModel, self).save()
-                return
+                raise exceptions.ValidationError('Module not found. Please check and send again.')
 
-            for modules in module_group.modules.all():
-                result_model = self.resultmodel_set.create(modules=modules)
-                result_model_list.append(result_model)
+        module_result = list()
 
-        result_dict = dict()
-        for result_model in result_model_list:
-            result_model.get_result()
-            result_dict[str(result_model.modules.name)] = str(result_model.result)
+        for module in module_set.all():
+            module_result.append(self.results.create(module=module))
 
-        self.result = json.dumps(result_dict)
+        for module in module_result:
+            module.get_result()
 
         super(ImageModel, self).save()
 
 
 class ResultModel(models.Model):
     result = models.TextField(null=True)
-    image = models.ForeignKey(ImageModel, on_delete=models.CASCADE)
-    modules = models.ForeignKey(ModuleModel)
+    image = models.ForeignKey(ImageModel, related_name='results', on_delete=models.CASCADE)
+    module = models.ForeignKey(ModuleModel)
+    task = None
+
+    class Meta:
+        unique_together = ('module', 'image')
+        ordering = ['module']
+
+    def __unicode__(self):
+        return "{0} : {1}".format(self.module.name, self.result)
 
     def save(self, *args, **kwargs):
         super(ResultModel, self).save(*args, **kwargs)
-        # Celery Delay
-        try:
-            self.task = tasks.post_image_and_get_result.delay(url=self.modules.url, image_path=self.image.image.path)
-        except:
-            self.task = None
+        self.set_task()
         super(ResultModel, self).save()
 
+    # Celery Delay
+    def set_task(self):
+        try:
+            self.task = tasks.post_image_and_get_result.delay(url=self.module.url, image_path=self.image.image.path)
+        except:
+            self.task = None
+
+    # Celery Get
     def get_result(self):
-        # Celery Get
         if self.task is not None:
-            self.result = self.task.get()
+            task_get = self.task.get()
+            self.result = ast.literal_eval(task_get)
         else:
             self.result = u"Module Error. Please contact the administrator"
+        super(ResultModel, self).save()
+
--- ModuleCommunicator/serializers.py
+++ ModuleCommunicator/serializers.py
@@ -4,10 +4,12 @@ from ModuleCommunicator.models import *
 
 
 class ImageSerializer(serializers.HyperlinkedModelSerializer):
+    results = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = ImageModel
-        fields = ('image', 'modules', 'token', 'uploaded_date', 'updated_date', 'result')
-        read_only_fields = ('token', 'uploaded_date', 'updated_date', 'result')
+        fields = ('image', 'modules', 'token', 'uploaded_date', 'updated_date', 'results')
+        read_only_fields = ('token', 'uploaded_date', 'updated_date', 'results')
 
 
 # class ResultSerializer(serializers.HyperlinkedModelSerializer):
