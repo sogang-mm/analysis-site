c768698bec4a7a1cf8c00ee789621dcbe58c866c
kwangsooshin <kepler92@daum.net>
2018-07-02 21:52:15 +0900

    Docker-compose Test

---
+++ docker/AnalysisSite/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+dockerize -wait tcp://postgres:5432 -timeout 20s
+sh migration.sh
+python -c "import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'AnalysisSite.settings'
+import django
+django.setup()
+from django.contrib.auth.management.commands.createsuperuser import get_user_model
+if not get_user_model().objects.filter(username='$DJANGO_SUPERUSER_USERNAME'): 
+    get_user_model()._default_manager.db_manager().create_superuser(username='$DJANGO_SUPERUSER_USERNAME', email='$DJANGO_SUPERUSER_EMAIL', password='$DJANGO_SUPERUSER_PASSWORD')"
+sh start_server.sh
+/bin/bash
